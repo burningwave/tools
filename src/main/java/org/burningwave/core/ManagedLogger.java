e62a4af0a5ca3c364d2bb0b607fb3259e7dff2ba
Roberto <roberto.gentili.1980@gmail.com>
2020-01-30 17:21:08 +0100

    Improved code

--- src/main/java/org/burningwave/core/ManagedLogger.java
+++ src/main/java/org/burningwave/core/ManagedLogger.java
@@ -82,37 +82,37 @@ public interface ManagedLogger {
 		public static void enableLogging(Class<?> client) {}
 		
 		public static void logError(Class<?> client, String message, Throwable exc) {
-			System.out.println(client.getClass().getName() + " - " + message + " - " + exc.getMessage());
+			System.out.println(client.getName() + " - " + message + " - " + exc.getMessage());
 		}
 		
 		public static void logError(Class<?> client, String message) {
-			System.out.println(client.getClass().getName() + " - " + message);
+			System.out.println(client.getName() + " - " + message);
 		}
 		
 		public static void logDebug(Class<?> client, String message) {
-			System.out.println(client.getClass().getName() + " - " + message);
+			System.out.println(client.getName() + " - " + message);
 		}
 		
 		public static void logDebug(Class<?> client, String message, Object... arguments) {
 			for (Object obj : arguments) {
 				message = message.replaceFirst("\\{\\}", clear(obj.toString()));
 			}
-			System.out.println(client.getClass().getName() + " - " + message);
+			System.out.println(client.getName() + " - " + message);
 		}
 		
 		public static void logInfo(Class<?> client, String message) {
-			System.out.println(client.getClass().getName() + " - " + message);
+			System.out.println(client.getName() + " - " + message);
 		}
 		
 		public static void logInfo(Class<?> client, String message, Object... arguments) {
 			for (Object obj : arguments) {
 				message = message.replaceFirst("\\{\\}", clear(obj.toString()));
 			}
-			System.out.println(client.getClass().getName() + " - " + message);
+			System.out.println(client.getName() + " - " + message);
 		}
 		
 		public static void logWarn(Class<?> client, String message) {
-			System.out.println(client.getClass().getName() + " - " + message);
+			System.out.println(client.getName() + " - " + message);
 		}
 		
 		public static void logWarn(Class<?> client, String message, Object... arguments) {
@@ -121,7 +121,7 @@ public interface ManagedLogger {
 					clear(obj.toString())
 				);
 			}
-			System.out.println(client.getClass() + " - " + message);
+			System.out.println(client + " - " + message);
 		}
 
 		protected static String clear(String text) {
