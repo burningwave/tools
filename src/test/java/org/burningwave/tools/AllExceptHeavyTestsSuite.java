51a43921194ae03c7d6d4ea6654ec9c2d8bbcf38
Roberto Gentili <roberto.gentili.1980@gmail.com>
2022-10-27 08:16:35 +0200

    Set tests order

--- src/test/java/org/burningwave/tools/AllExceptHeavyTestsSuite.java
+++ src/test/java/org/burningwave/tools/AllExceptHeavyTestsSuite.java
@@ -2,12 +2,17 @@ package org.burningwave.tools;
 
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.ExcludeTags;
-import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.SelectClasses;
 import org.junit.runner.RunWith;
 
 @RunWith(JUnitPlatform.class)
-@SelectPackages("org.burningwave.tools")
+//@SelectPackages("org.burningwave.tools")
 @ExcludeTags("Heavy")
+@SelectClasses({
+	CapturerTest.class,
+	TwoPassCapturerTest.class,
+	HostNameForIPMapperTest.class
+})
 public class AllExceptHeavyTestsSuite {
 
 }
